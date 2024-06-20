a6d5f1d675bc5a6b17daf7b7bad3156e77ae314d
Skarabela <martin.skarabela@chyron.com>
2024-06-20 16:53:10 +0200

    feat(dummy-project) eslint and prettier configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  printWidth: 160,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
